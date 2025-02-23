abe59f8c69c52902fe5c91f349d529629ed9b59d
MightyWolf1029 <mightywolf1029@gmail.com>
2025-02-23 18:45:01 -0500

    Remove AP item messages from OoTR, create Messages.py to stand in for the OoTR file

---
+++ worlds/oot/Messages.py
@@ -0,0 +1,5 @@
+from .OoTR.Messages import *
+
+# Add the Archipelago item messages
+ITEM_MESSAGES[0x9097] = "\x08You got an \x05\x41Archipelago item\x05\x40!\x01It seems \x05\x41important\x05\x40!"
+ITEM_MESSAGES[0x9098] = "\x08You got an \x05\x43Archipelago item\x05\x40!\x01Doesn't seem like it's needed."
