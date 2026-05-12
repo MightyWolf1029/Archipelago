6bb4815389d5cdfdd19b1e38a872aed825d2a494
MightyWolf <mightywolf1029@gmail.com>
2026-05-11 20:09:00 -0400

    Revert OoTR/Region.py to match OoTR, update Region.py to inherit from OoTR's Region

--- worlds/oot/OoTR/Region.py
+++ worlds/oot/OoTR/Region.py
@@ -1,10 +1,6 @@
-from enum import unique, Enum
-
-from BaseClasses import Region, MultiWorld
-from .Hints import HintArea
+from enum import Enum, unique
 
 
-# copied from OoT-Randomizer/Region.py
 @unique
 class RegionType(Enum):
 
@@ -19,6 +15,7 @@ class RegionType(Enum):
         """Shorthand for checking if Interior or Dungeon"""
         return self in (RegionType.Interior, RegionType.Dungeon, RegionType.Grotto)
 
+
 # Pretends to be an enum, but when the values are raw ints, it's much faster
 class TimeOfDay(object):
     NONE = 0
@@ -27,57 +24,127 @@ class TimeOfDay(object):
     ALL = DAY | DAMPE
 
 
+class Region(object):
 
-
-class OOTRegion(Region):
-    game: str = "Ocarina of Time"
-
-    def __init__(self, name: str, player: int, multiworld: MultiWorld):
-        super(OOTRegion, self).__init__(name, player, multiworld)
-        self._oot_hint = None
-        self.alt_hint = None
+    def __init__(self, name, type=RegionType.Overworld):
+        self.name = name
+        self.type = type
+        self.entrances = []
+        self.exits = []
+        self.locations = []
+        self.dungeon = None
+        self.world = None
+        self.hint_name = None
+        self.alt_hint_name = None
         self.price = None
+        self.world = None
         self.time_passes = False
         self.provides_time = TimeOfDay.NONE
         self.scene = None
-        self.dungeon = None
-        self.pretty_name = None
-        self.font_color = None
         self.is_boss_room = False
 
-    # This is too generic of a name to risk not breaking in the future.
-    # This lets us possibly switch it out later if AP starts using it.
+
+    def copy(self, new_world):
+        new_region = Region(self.name, self.type)
+        new_region.world = new_world
+        new_region.price = self.price
+        new_region.hint_name = self.hint_name
+        new_region.alt_hint_name = self.alt_hint_name
+        new_region.time_passes = self.time_passes
+        new_region.provides_time = self.provides_time
+        new_region.scene = self.scene
+
+        if self.dungeon:
+            new_region.dungeon = self.dungeon.name
+        new_region.locations = [location.copy(new_region) for location in self.locations]
+        new_region.exits = [exit.copy(new_region) for exit in self.exits]
+
+        return new_region
+
+
     @property
     def hint(self):
-        return self._oot_hint
+        from Hints import HintArea
 
-    @hint.setter
-    def hint(self, value):
-        self._oot_hint = value
+        if self.hint_name is not None:
+            return HintArea[self.hint_name]
+        if self.dungeon:
+            return self.dungeon.hint
+
+    @property
+    def alt_hint(self):
+        from Hints import HintArea
+
+        if self.alt_hint_name is not None:
+            return HintArea[self.alt_hint_name]
+
+
+    def can_fill(self, item, manual=False):
+        if not manual and self.world.settings.empty_dungeons_mode != 'none' and item.dungeonitem:
+            # An empty dungeon can only store its own dungeon items
+            if self.dungeon and self.dungeon.world.empty_dungeons[self.dungeon.name].empty:
+                return self.dungeon.is_dungeon_item(item) and item.world.id == self.world.id
+            # Items from empty dungeons can only be in their own dungeons
+            for dungeon in item.world.dungeons:
+                if item.world.empty_dungeons[dungeon.name].empty and dungeon.is_dungeon_item(item):
+                    return False
+
+        from Hints import HintArea
+
+        is_self_dungeon_restricted = False
+        is_self_region_restricted = None
+        is_hint_color_restricted = None
+        is_dungeon_restricted = False
+        is_overworld_restricted = False
+
+        if item.type in ['Map', 'Compass', 'SmallKey', 'HideoutSmallKey', 'BossKey', 'GanonBossKey']:
+            shuffle_setting = (self.world.settings.shuffle_mapcompass if item.type in ['Map', 'Compass'] else
+                               self.world.settings.shuffle_smallkeys if item.type == 'SmallKey' else
+                               self.world.settings.shuffle_hideoutkeys if item.type == 'HideoutSmallKey' else
+                               self.world.settings.shuffle_bosskeys if item.type == 'BossKey' else
+                               self.world.settings.shuffle_ganon_bosskey if item.type == 'GanonBossKey' else None)
+
+            is_self_dungeon_restricted = shuffle_setting in ['dungeon', 'vanilla'] and item.type != 'HideoutSmallKey'
+            is_self_region_restricted = [HintArea.GERUDO_FORTRESS] if shuffle_setting == 'fortress' else None
+            is_hint_color_restricted = [HintArea.for_dungeon(item.name).color] if shuffle_setting == 'regional' else None
+            is_dungeon_restricted = shuffle_setting == 'any_dungeon'
+            is_overworld_restricted = shuffle_setting == 'overworld'
+
+        if is_self_dungeon_restricted and not manual:
+            hint_area = HintArea.at(self)
+            return hint_area.is_dungeon and hint_area.is_dungeon_item(item) and item.world.id == self.world.id
+
+        if is_self_region_restricted and not manual:
+            return HintArea.at(self) in is_self_region_restricted and item.world.id == self.world.id
+
+        if is_hint_color_restricted and not manual:
+            return HintArea.at(self).color in is_hint_color_restricted
+
+        if is_dungeon_restricted and not manual:
+            return HintArea.at(self).is_dungeon
+
+        if is_overworld_restricted and not manual:
+            return not HintArea.at(self).is_dungeon
+
+        if item.name == 'Triforce Piece':
+            return item.world.id == self.world.id
+
+        return True
+
 
     def get_scene(self):
-        if self.scene: 
+        if self.scene:
             return self.scene
-        elif self.dungeon: 
+        elif self.dungeon:
             return self.dungeon.name
-        else: 
+        else:
             return None
 
-    def can_reach(self, state):
-        if state._oot_stale[self.player]:
-            stored_age = state.age[self.player]
-            state._oot_update_age_reachable_regions(self.player)
-            state.age[self.player] = stored_age
-        if state.age[self.player] == 'child': 
-            return self in state.child_reachable_regions[self.player]
-        elif state.age[self.player] == 'adult': 
-            return self in state.adult_reachable_regions[self.player]
-        else: # we don't care about age
-            return self in state.child_reachable_regions[self.player] or self in state.adult_reachable_regions[self.player]
 
-    def set_hint_data(self, hint):
-        if self.dungeon:
-            self._oot_hint = HintArea.for_dungeon(self.dungeon)
-        else:
-            self._oot_hint = HintArea[hint]
-        self._hint_text = str(self._oot_hint)
+    def __str__(self):
+        return str(self.__unicode__())
+
+
+    def __unicode__(self):
+        return '%s' % self.name
+
--- worlds/oot/Region.py
+++ worlds/oot/Region.py
@@ -1,22 +1,22 @@
-from BaseClasses import Region, MultiWorld
+from BaseClasses import Region as APRegion, MultiWorld
 from .OoTR.Hints import HintArea
-from .OoTR.Region import TimeOfDay
+from .OoTR.Region import Region as OoTRRegion, TimeOfDay
 
-class OOTRegion(Region):
+class OOTRegion(APRegion, OoTRRegion):
+    """
+    A class to represent a region in OoT. Inherits from AP's base ``Region`` class as well as
+    OoTR's ``Region``.
+    """
     game: str = "Ocarina of Time"
 
     def __init__(self, name: str, player: int, multiworld: MultiWorld):
-        super(OOTRegion, self).__init__(name, player, multiworld)
+        APRegion.__init__(self, name, player, multiworld)
+        OoTRRegion.__init__(self, name, None) # AP doesn't use OoTRRegion's type property
         self._oot_hint = None
-        self.alt_hint = None
-        self.price = None
-        self.time_passes = False
-        self.provides_time = TimeOfDay.NONE
-        self.scene = None
+        self._alt_hint = None
         self.dungeon = None
         self.pretty_name = None
         self.font_color = None
-        self.is_boss_room = False
 
     # This is too generic of a name to risk not breaking in the future.
     # This lets us possibly switch it out later if AP starts using it.
@@ -28,15 +28,22 @@ class OOTRegion(Region):
     def hint(self, value):
         self._oot_hint = value
 
-    def get_scene(self):
-        if self.scene:
-            return self.scene
-        elif self.dungeon:
-            return self.dungeon.name
-        else:
-            return None
+    @property
+    def alt_hint(self):
+        return self._alt_hint
+
+    @alt_hint.setter
+    def alt_hint(self, value):
+        self._alt_hint = value
 
     def can_reach(self, state):
+        """
+        A helper function to determine if this ``OOTRegion`` can be reached.
+        Args:
+            state: The current state
+        Returns:
+            ``True`` if this ``OOTRegion`` can be reached, ``False`` otherwise
+        """
         if state._oot_stale[self.player]:
             stored_age = state.age[self.player]
             state._oot_update_age_reachable_regions(self.player)
@@ -49,6 +56,11 @@ class OOTRegion(Region):
             return self in state.child_reachable_regions[self.player] or self in state.adult_reachable_regions[self.player]
 
     def set_hint_data(self, hint):
+        """
+        A helper function to set this ``OOTRegion``'s hint data.
+        Args:
+            hint: The hint data to set.
+        """
         if self.dungeon:
             self._oot_hint = HintArea.for_dungeon(self.dungeon)
         else:
